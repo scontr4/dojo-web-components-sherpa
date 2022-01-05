298eed297c9a015d5443c7818c257f15b04aa6d4
MauricioAcosta <amacostapulido@gmail.com>
2022-01-05 11:53:02 -0500

    update template angular 13 FMACC-324

--- ci-cd/install-dependencies.sh
+++ ci-cd/install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eux
 rm ~/.npmrc | true
-curl -u "${ARTIFACTORY_READER_USER}:${ARTIFACTORY_READER_PASSWORD}" 'https://bbogdigital.jfrog.io/bbogdigital/api/npm/auth' >> ~/.npmrc
+curl -u "${ARTIFACTORY_READER_USER}:${ARTIFACTORY_READER_API_KEY}" 'https://bbogdigital.jfrog.io/bbogdigital/api/npm/auth' >> ~/.npmrc
 
 npm install
