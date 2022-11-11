68d670a89ee7b85fb7de9bd6c693962739480baf
JUAN SEBASTIAN SANCHEZ L <jsan103@bancodebogota.com.co>
2022-11-11 15:29:34 -0500

    dependencies v1

--- ci-cd/install-dependencies.sh
+++ ci-cd/install-dependencies.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eux
-rm ~/.npmrc || true
+rm ~/.npmrc | true
 curl -u "${ARTIFACTORY_READER_USER}:${ARTIFACTORY_READER_API_KEY}" 'https://bbogdigital.jfrog.io/bbogdigital/api/npm/auth' >> ~/.npmrc
 
-npm install || exit 1
-git reset --hard || exit 1
+npm install
