79d9c4d344cb3d3536a65e62dddbb675a380b034
Oscar Beltrán <obeltra@bancodebogota.com.co>
2022-11-12 09:10:42 -0500

    use node version 16.16.0

--- sherpa-scanner.config.js
+++ sherpa-scanner.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   team: "TRANSVERSALES",
   branch: "QA",
-  repository: "DESIGN-SYSTEM",
+  repository: "ANGULAR-TEMPLATE",
   framework: "ANGULAR",
-  version: "versionlibreria",
+  version: "1.1.1",
   crawlFrom: "./src",
   globs: ["**/!(*.test|*.spec).@(xhtml|html)?(x)"],
   url: "https://3mt8atlcre.execute-api.us-east-1.amazonaws.com/v1/v1/utilities/web-components/use-report",
