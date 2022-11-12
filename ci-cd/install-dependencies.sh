f09d1a39edc3427fd08e6d593f4ff3cae29747e9
Oscar Beltrán <obeltra@bancodebogota.com.co>
2022-11-12 08:36:22 -0500

    rebuild package lock

--- ci-cd/install-dependencies.sh
+++ ci-cd/install-dependencies.sh
@@ -14,10 +14,5 @@ curl -u "${ARTIFACTORY_READER_USER}:${ARTIFACTORY_READER_API_KEY}" 'https://bbog
 sed -i 's#_auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:_auth#g' ~/.npmrc
 sed -i 's#always-auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:always-auth#g' ~/.npmrc
 sed -i 's#email#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:email#g' ~/.npmrc
-npm ci || exit 1
-
-
-echo --- cat
-cat ~/.npmrc
 
 npm ci || exit 1
