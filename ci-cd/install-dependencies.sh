f830dce7c6edc677493a6b38f5d8b39114429631
Oscar Beltrán <obeltra@bancodebogota.com.co>
2022-11-12 08:09:28 -0500

    recreate package-lock

--- ci-cd/install-dependencies.sh
+++ ci-cd/install-dependencies.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+node --version
+npm --version
+
 set -eux
 rm ~/.npmrc | true
 curl -u "${ARTIFACTORY_READER_USER}:${ARTIFACTORY_READER_API_KEY}" 'https://bbogdigital.jfrog.io/bbogdigital/api/npm/auth' >> ~/.npmrc
-sed -i 's#_auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:_auth#g' ~/.npmrc
-sed -i 's#always-auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:always-auth#g' ~/.npmrc
-sed -i 's#email#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:email#g' ~/.npmrc
+sed -i '' 's#_auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:_auth#g' ~/.npmrc
+sed -i '' 's#always-auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:always-auth#g' ~/.npmrc
+sed -i '' 's#email#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:email#g' ~/.npmrc
 npm ci || exit 1
 
 
