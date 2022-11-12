e96b2b617ced8b1f811bed933537026c2a1ccc5c
Oscar Beltrán <obeltra@bancodebogota.com.co>
2022-11-11 21:51:18 -0500

    try to use bash script

--- ci-cd/install-dependencies.sh
+++ ci-cd/install-dependencies.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eux
-rm ~/.npmrc || true
+rm ~/.npmrc | true
 curl -u "${ARTIFACTORY_READER_USER}:${ARTIFACTORY_READER_API_KEY}" 'https://bbogdigital.jfrog.io/bbogdigital/api/npm/auth' >> ~/.npmrc
-npm install || exit 1
-git reset --hard || exit 1
+sed -i 's#_auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:_auth#g' ~/.npmrc
+sed -i 's#always-auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:always-auth#g' ~/.npmrc
+sed -i 's#email#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:email#g' ~/.npmrc
+npm ci || exit 1
