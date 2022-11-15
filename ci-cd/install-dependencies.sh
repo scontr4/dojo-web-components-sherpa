74c4f1ee62d47a21ca75eae6c3a8c6b51a020b52
JUAN SEBASTIAN SANCHEZ L <jsan103@bancodebogota.com.co>
2022-11-15 11:35:53 -0500

    npmrc 3

--- ci-cd/install-dependencies.sh
+++ ci-cd/install-dependencies.sh
@@ -5,6 +5,7 @@ echo NPM Version: $(npm --version)
 
 set -eux
 rm .npmrc | true
+nano .npmrc | true
 curl -u "${ARTIFACTORY_READER_USER}:${ARTIFACTORY_READER_API_KEY}" 'https://bbogdigital.jfrog.io/bbogdigital/api/npm/auth' >> .npmrc
 # for macos only
 #sed -i '' 's#_auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:_auth#g' ~/.npmrc
