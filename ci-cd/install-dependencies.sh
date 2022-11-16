9e9f8f996a6b67abf3ee9c3d7e305e875d83c15d
JUAN SEBASTIAN SANCHEZ L <jsan103@bancodebogota.com.co>
2022-11-16 10:51:04 -0500

    test creds

--- ci-cd/install-dependencies.sh
+++ ci-cd/install-dependencies.sh
@@ -5,16 +5,12 @@ echo NPM Version: $(npm --version)
 set -eux
 rm ~/.npmrc | true
 curl -u "${ARTIFACTORY_READER_USER}:${ARTIFACTORY_READER_API_KEY}" 'https://bbogdigital.jfrog.io/bbogdigital/api/npm/auth' >> ~/.npmrc
-cat /home/runner/work/bbog-dig-dt-angular-template-web-ui/bbog-dig-dt-angular-template-web-ui/.npmrc
+echo -1_---- ~/.npmrc -----
+cat ~/.npmrc
 
-# for macos only
-#sed -i '' 's#_auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:_auth#g' ~/.npmrc
-#sed -i '' 's#always-auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:always-auth#g' ~/.npmrc
-#sed -i '' 's#email#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:email#g' ~/.npmrc
-
-sed -i 's#_auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:_auth#g' /home/runner/work/bbog-dig-dt-angular-template-web-ui/bbog-dig-dt-angular-template-web-ui/.npmrc
-sed -i 's#always-auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:always-auth#g' /home/runner/work/bbog-dig-dt-angular-template-web-ui/bbog-dig-dt-angular-template-web-ui/.npmrc
-sed -i 's#email#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:email#g' /home/runner/work/bbog-dig-dt-angular-template-web-ui/bbog-dig-dt-angular-template-web-ui/.npmrc
+sed -i 's#_auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:_auth#g' ~/npmrc
+sed -i 's#always-auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:always-auth#g' ~/npmrc
+sed -i 's#email#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:email#g' ~/npmrc
 
 npm ci || exit 1
 
