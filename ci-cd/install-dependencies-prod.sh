823386ee09a788a40cdf83045b24222daa1ce174
JUAN SEBASTIAN SANCHEZ L <jsan103@bancodebogota.com.co>
2022-11-16 11:59:15 -0500

    .npmrc 10

--- ci-cd/install-dependencies-prod.sh
+++ ci-cd/install-dependencies-prod.sh
@@ -6,12 +6,8 @@ echo NPM Version: $(npm --version)
 set -eux
 rm ~/.npmrc | true
 curl -u "${ARTIFACTORY_READER_USER}:${ARTIFACTORY_READER_API_KEY}" 'https://bbogdigital.jfrog.io/bbogdigital/api/npm/auth' >> ~/.npmrc
-# for macos only
-#sed -i '' 's#_auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:_auth#g' ~/.npmrc
-#sed -i '' 's#always-auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:always-auth#g' ~/.npmrc
-#sed -i '' 's#email#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:email#g' ~/.npmrc
-
 sed -i 's#_auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:_auth#g' ~/.npmrc
 sed -i 's#always-auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:always-auth#g' ~/.npmrc
 sed -i 's#email#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:email#g' ~/.npmrc
+
 npm ci --only=prod || exit 1
--- ci-cd/install-dependencies.sh
+++ ci-cd/install-dependencies.sh
@@ -2,13 +2,12 @@
 
 echo Node Version: $(node --version)
 echo NPM Version: $(npm --version)
+
 set -eux
 rm ~/.npmrc | true
 curl -u "${ARTIFACTORY_READER_USER}:${ARTIFACTORY_READER_API_KEY}" 'https://bbogdigital.jfrog.io/bbogdigital/api/npm/auth' >> ~/.npmrc
-echo -1_---- ~/.npmrc -----
-cat ~/.npmrc
-# or replace ARTIFACTORY_READER_API_KEY by ARTIFACTORY_READER_PASSWORD if not exist
 sed -i 's#_auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:_auth#g' ~/.npmrc
 sed -i 's#always-auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:always-auth#g' ~/.npmrc
 sed -i 's#email#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:email#g' ~/.npmrc
-npm ci # or npm install
+
+npm ci || exit 1
