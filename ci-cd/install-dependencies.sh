6df3902c8c00a0b00dc5d50783c148616016b622
JUAN SEBASTIAN SANCHEZ L <jsan103@bancodebogota.com.co>
2022-11-16 11:31:28 -0500

    install-dependencies with sed

--- ci-cd/install-dependencies.sh
+++ ci-cd/install-dependencies.sh
@@ -7,6 +7,8 @@ rm ~/.npmrc | true
 curl -u "${ARTIFACTORY_READER_USER}:${ARTIFACTORY_READER_API_KEY}" 'https://bbogdigital.jfrog.io/bbogdigital/api/npm/auth' >> ~/.npmrc
 echo -1_---- ~/.npmrc -----
 cat ~/.npmrc
-
-npm ci || exit 1
-
+# or replace ARTIFACTORY_READER_API_KEY by ARTIFACTORY_READER_PASSWORD if not exist
+sed -i 's#_auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:_auth#g' ~/.npmrc
+sed -i 's#always-auth#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:always-auth#g' ~/.npmrc
+sed -i 's#email#//bbogdigital.jfrog.io/bbogdigital/api/npm/npm-bbta/:email#g' ~/.npmrc
+npm ci # or npm install
