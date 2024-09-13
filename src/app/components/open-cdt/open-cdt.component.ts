6d6fdefb607ef717437d1e22f45a9000a88af621
jvega18 <jvega18@bancodebogota.com.co>
2024-09-13 12:21:20 -0500

    Add redirection buttom

--- src/app/components/open-cdt/open-cdt.component.ts
+++ src/app/components/open-cdt/open-cdt.component.ts
@@ -1,9 +1,10 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-open-cdt',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './open-cdt.component.html',
   styleUrl: './open-cdt.component.scss',
