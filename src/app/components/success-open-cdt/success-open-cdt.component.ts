8ecb5b0e02d7c586b7b506c480111ef7e6e9cb33
scontr4 <scontr4@bancodebogota.com.co>
2024-09-13 12:18:43 -0500

    skeleton of success component

--- src/app/components/success-open-cdt/success-open-cdt.component.ts
+++ src/app/components/success-open-cdt/success-open-cdt.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-success-open-cdt',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './success-open-cdt.component.html',
   styleUrl: './success-open-cdt.component.scss'
 })
