cae255eacea583345c0359ecfc724dfdf1f9de98
jvega18 <jvega18@bancodebogota.com.co>
2024-09-13 11:59:22 -0500

    Set open cdt styles

--- src/app/components/open-cdt/open-cdt.component.ts
+++ src/app/components/open-cdt/open-cdt.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-open-cdt',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './open-cdt.component.html',
-  styleUrl: './open-cdt.component.scss'
+  styleUrl: './open-cdt.component.scss',
 })
-export class OpenCdtComponent {
-
-}
+export class OpenCdtComponent {}
